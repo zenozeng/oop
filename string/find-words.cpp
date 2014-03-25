7a04933951dd18939062ee701124d50c40a58ece
Zeno Zeng <zenoofzeng@gmail.com>
2014-03-25 13:48:10 +0800

    init find-words.cpp

--- string/find-words.cpp
+++ string/find-words.cpp
@@ -1,2 +1,18 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
 int main() {
+    cout << "Find Words containing the given substring, and print it in the order of alphabet";
+
+    ifstream file("TEXT");
+
+    if (file.is_open()) {
+        file.close();
+    } else {
+        cout << "Fail to open file";
+    }
+
+    return 0;
 }
